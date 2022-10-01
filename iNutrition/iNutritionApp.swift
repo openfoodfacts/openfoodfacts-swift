28bbc2f16da5451104e5beb3e5938ba5937f6b81
Kedar27 <30876876+Kedar-27@users.noreply.github.com>
2022-10-01 19:44:47 +0530

    Create - Base project setup in swiftui.

---
+++ iNutrition/iNutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNutritionApp.swift
+//  iNutrition
+//
+//  Created by Kedar Sukerkar on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iNutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
